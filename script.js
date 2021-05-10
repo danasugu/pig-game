5cc3d099f7749eb06121c0f796cfdc65197e2882
Dana Sugu <dana.sugu@gmail.com>
2021-05-10 10:39:13 +0300

    rolling

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// rolling dice functionality
+btnRoll.addEventListener('click', function() {
+	// 1.generate random dice roll
+	const dice = Math.trunc(Math.random() * 6) + 1;
+	// 2. display dice
+	// 3. check for dice 1: if true switch to next player
+});
