f718dab8e90b9f0e37eb5a461aaa1eaf62f4c578
Dana Sugu <dana.sugu@gmail.com>
2021-05-12 08:54:04 +0300

    cont

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,6 +15,8 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 // rolling dice functionality
 btnRoll.addEventListener('click', function() {
 	// 1.generate random dice roll
@@ -22,4 +26,12 @@ btnRoll.addEventListener('click', function() {
 	diceEl.src = `dice-${dice}.png`;
 
 	// 3. check for dice 1: if true switch to next player
+	if (dice !== 1) {
+		// add dice number to current score
+		// currentScore = currentScore + dice;
+		currentScore += dice;
+		current0El.textContent = currentScore;
+		// switch to next player
+	} else {
+	}
 });
