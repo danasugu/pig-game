68fa3f1d40b64c0d8bc6c98e4b42b027d7dafecf
Dana Sugu <dana.sugu@gmail.com>
2021-05-13 20:01:48 +0300

    cont.

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [ 0, 0 ];
 let currentScore = 0;
+let activePlayer = 0;
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function() {
@@ -31,7 +33,7 @@ btnRoll.addEventListener('click', function() {
 		// currentScore = currentScore + dice;
 		currentScore += dice;
 		current0El.textContent = currentScore;
-		// loose score and switch to next player
+		// loose all score and switch to next player
 	} else {
 	}
 });
